2d8c4b2cffd4f6fcccf8ca0081d11a51ad343efd
Prince Sharma <hit.princeraj@gmail.com>
2025-09-17 12:39:04 +0530

    Add login functionality to authentication controller and route

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,7 +7,6 @@ async function registerContrller(req, res) {
     const { fullName = {}, email, password } = req.body || {};
     const { firstName, lastName } = fullName;
 
-
     //check user exists
     const isUseralreadyExist = await userModel.findOne({
       email
@@ -56,6 +55,46 @@ async function registerContrller(req, res) {
 
 }
 
+async function loginController(req, res) {
+  try {
+    const { email, password } = req.body
+    // check userExist
+    const user = await userModel.findOne({
+      email
+    })
+    if (!user) {
+      return res.status(500).json({
+        message: "invalid user"
+      })
+    }
+
+    // check validPassword
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      return res.json({
+        message: "incorrect password pls try again"
+      })
+    }
+    // email and password is correct then set token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    res.cookie("token", token)
+    res.status(200).json({
+      message: "user logged in successfully",
+      user: {
+        email: user.email,
+        _id: user._id,
+        fullName: user.fullName
+      }
+    })
+  } catch (err) {
+    console.log("error", err)
+    res.status(500).json({
+      message: "server error",
+      err: err.message
+    })
+  }
+
+}
 
 
-export default { registerContrller }
+export default { registerContrller, loginController }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,7 @@ import authController from "../controllers/auth.controller.js"
 const router = express.Router()
 
 router.use('/register', authController.registerContrller)
+router.use('/login', authController.loginController)
 
 
 export default router
